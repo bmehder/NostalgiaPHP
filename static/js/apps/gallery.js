e57cb6a586f2fb5e609cffddf767e45cb0da40ca
bmehder <bmehder@gmail.com>
2025-09-27 14:40:46 +0100

    hero button

--- static/js/apps/gallery.js
+++ static/js/apps/gallery.js
@@ -1,20 +1,4 @@
 // // gallery.js
-// const createGallery = domTarget => {
-// 	const images = domTarget.getAttribute('data-gallery')
-
-// 	domTarget.innerHTML = images
-// 		? images
-// 				.split(',')
-// 				.map(
-// 					image =>
-// 						`<img src="/static/${image.trim()}" loading="lazy" />`
-// 				)
-// 				.join('')
-// 		: '<p>No images found</p>'
-// }
-
-// // Create image elements for each gallery on the page.
-// document.querySelectorAll('[data-gallery]').forEach(createGallery)
 
 // Utility: normalize to /static/media
 const normalizeSrc = raw => raw.trim() || null
