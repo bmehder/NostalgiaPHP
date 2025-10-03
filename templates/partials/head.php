55282f5df1459bae29d407c0d47ddf48c56dc666
bmehder <bmehder@gmail.com>
2025-10-03 04:30:16 +0100

    favicons

--- templates/partials/head.php
+++ templates/partials/head.php
@@ -51,6 +51,17 @@ $pathOnly = parse_url($reqUri, PHP_URL_PATH) ?? '/';
 
   <link rel="canonical" href="<?= htmlspecialchars($canonical, ENT_QUOTES, 'UTF-8') ?>">
 
+  <!-- Favicon core -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png">
+  <link rel="manifest" href="/static/favicons/site.webmanifest">
+  <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#000000">
+  <link rel="shortcut icon" href="/static/favicons/favicon.ico">
+  <meta name="msapplication-TileColor" content="#000000">
+  <meta name="msapplication-config" content="/static/favicons/browserconfig.xml">
+  <meta name="theme-color" content="#000000">
+
   <?php
   // ----- rel=prev/next for paginated collection lists -----
   if (!empty($meta['pagination'])):
